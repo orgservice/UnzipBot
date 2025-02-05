bc1bdcf7d62bd448487dceb4e6f10aa3d459112b
orgservice <146081596+orgservice@users.noreply.github.com>
2025-02-05 22:17:41 +0530

    Update config.py

--- Unzip/config.py
+++ Unzip/config.py
@@ -2,8 +2,8 @@ import os
 
 class Config(object):
      
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", ))
-    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6636023413:AAHMmRz5x48PbkGJuYItUmXKtU8RpjCkBRQ")
+    API_ID = int(os.environ.get("API_ID", "25465082"))
+    API_HASH = os.environ.get("API_HASH", "4a6b5e40c8bc08c8af09add6cca23b18")
     
     
